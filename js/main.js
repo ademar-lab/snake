7a4a5f6fe34d4b6bf4967f76171802e1df91b1b3
Ademar <ademardeleonvazquez@gmail.com>
2023-09-26 14:08:25 -0600

    The initial snake is rendered by JS

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 const stopButton = document.querySelector('.stop');
 const startButton = document.querySelector('.start');
+const gameContainer = document.querySelector('.gameContainer')
+const xInitialCoordinate = 13;
+const yInitialCoordinate = 13;
+
 let run;
 
 startButton.addEventListener('click', startGame);
 stopButton.addEventListener('click', stopGame);
+
+function displaySnake(x, y) {
+    let snakeSquare = document.createElement('div');
+    snakeSquare.classList.add('activeCoordinate');
+    setGridCoordinates(snakeSquare, x, y);
+    gameContainer.appendChild(snakeSquare);
+}
+
+function setGridCoordinates(square, x, y) {
+    square.style.gridRow = `${x}`;
+    square.style.gridColumn = `${y}`;
+}
 
 function stopGame() {
     run = false;
@@ -11,6 +27,8 @@ function stopGame() {
 
 function startGame() {
     run = true;
+    displaySnake(xInitialCoordinate, yInitialCoordinate);
+
     setInterval(() => {
         if(run) {
             console.log('Running');
