966ec255fac44f47ca614df088ee292d4d96e369
Ademar <ademardeleonvazquez@gmail.com>
2023-09-26 17:46:00 -0600

    The snake is now dislpayed on the screen and is able to move using the arrow keys

--- js/main.js
+++ js/main.js
@@ -5,11 +5,15 @@ const xInitialCoordinate = 13;
 const yInitialCoordinate = 13;
 
 let run;
-let velocity = 1000;
+let velocity = 300;
 let snake = [];
+let direction;
+// Default direction
+direction = 'ArrowUp';
 
 startButton.addEventListener('click', startGame);
 stopButton.addEventListener('click', stopGame);
+window.addEventListener("keydown", (e) => {direction = e.key });
 
 class SnakeSquare {
     constructor(squareDiv, x, y) {
@@ -19,6 +23,33 @@ class SnakeSquare {
     }
 }
 
+// Motion functions
+function moveUp(snake) {
+    for (let i = 0; i < snake.length; i++) {
+        snake[i].y--;
+        setGridCoordinates(snake[i].squareDiv, snake[i].x, snake[i].y);
+    }
+}
+function moveDown(snake) {
+    for (let i = 0; i < snake.length; i++) {
+        snake[i].y++;
+        setGridCoordinates(snake[i].squareDiv, snake[i].x, snake[i].y);
+    }
+}
+function moveRight(snake) {
+    for (let i = 0; i < snake.length; i++) {
+        snake[i].x++;
+        setGridCoordinates(snake[i].squareDiv, snake[i].x, snake[i].y);
+    }
+}
+function moveLeft(snake) {
+    for (let i = 0; i < snake.length; i++) {
+        snake[i].x--;
+        setGridCoordinates(snake[i].squareDiv, snake[i].x, snake[i].y);
+    }
+}
+
+// General functions
 function createSnakeSquareDiv(x, y) {
     let snakeSquareDiv = document.createElement('div');
 
@@ -35,13 +66,6 @@ function setGridCoordinates(squareDiv, x, y) {
     squareDiv.style.gridColumn = `${x}`;
 }
 
-function moveForward(snake) {
-    for (let i = 0; i < snake.length; i++) {
-        snake[i].y--;
-        setGridCoordinates(snake[i].squareDiv, snake[i].x, snake[i].y);
-    }
-}
-
 function stopGame() {
     run = false;
 }
@@ -52,7 +76,21 @@ function startGame() {
 
     setInterval(() => {
         if(run) {
-            moveForward(snake);
+            console.log('running');
+            switch (direction) {
+                case 'ArrowDown':
+                    moveDown(snake);
+                    break;
+                case 'ArrowUp':
+                    moveUp(snake);
+                    break;
+                case 'ArrowLeft':
+                    moveLeft(snake);
+                    break;
+                case 'ArrowRight':
+                    moveRight(snake);
+                    break;
+            }
         } 
         else {
             clearInterval();
